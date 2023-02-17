a565d40f97e77b7d80c6b3a936d34bb987f129d0
Max Chuyko <Skovorodka150@mail.ru>
2023-02-17 03:01:33 +0300

    life cycle with toasts and logs

--- app/src/main/java/com/example/prac1/MainActivity.java
+++ app/src/main/java/com/example/prac1/MainActivity.java
@@ -3,15 +3,60 @@ package com.example.prac1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"OnCreate",Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"info create");
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        Log.e(TAG,"error start");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"otladka resume");
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        Log.v(TAG,"podrobno pause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"info stop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "OnRestart", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"info restart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"info Destroy");
+    }
 }
